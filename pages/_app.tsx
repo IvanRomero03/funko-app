d5f15a19327a18bcecbfd6f503038205d8bf7b54
IvanRomero03 <i.wells.ar@gmail.com>
2022-10-28 01:31:54 -0500

    semana 18 proy

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
+}
